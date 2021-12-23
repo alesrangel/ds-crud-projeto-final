78db7e936fce2e1c710130d33670fa31ab6d0191
alesrangel <alesrangel15@gmail.com>
2021-12-23 00:19:42 -0300

    projeto final crud

---
+++ projetofinal/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria','00000000000', 4000.00, TIMESTAMP WITH TIME ZONE '1992-07-21T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Enzo','11111111111', 3000.00, TIMESTAMP WITH TIME ZONE '1998-02-20T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Clara','22222222222', 6000.00, TIMESTAMP WITH TIME ZONE '1998-03-2T10:30:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe','33333333333', 1000.00, TIMESTAMP WITH TIME ZONE '2000-04-4T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rodrigo','44444444444', 5000.00, TIMESTAMP WITH TIME ZONE '1989-05-11T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas','66666666666', 9000.00, TIMESTAMP WITH TIME ZONE '1993-09-10T10:30:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ezequiel','777777777777', 3500.00, TIMESTAMP WITH TIME ZONE '1995-12-30T10:30:00Z' , 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Samuel','88888888888', 5500.00, TIMESTAMP WITH TIME ZONE  '1994-01-12T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Letícia','99999999999', 7500.00, TIMESTAMP WITH TIME ZONE '1998-06-20T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vanessa','12345678912', 7500.00, TIMESTAMP WITH TIME ZONE '1998-06-20T10:30:00Z', 0);
+
